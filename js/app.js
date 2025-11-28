1148601fef7a480adef98c52145b7d7ebdb3cb31
karrim0 <karimhnfy1@gmail.com>
2025-11-28 15:38:53 +0200

    add static UI layout for dashboard sections

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// ============================
+// DOM ELEMENTS
+// ============================
+
+//  Form
+const transactionForm = document.getElementById("transaction-form");
+
+// RadioButtons (income / expense)
+const typeInputs = document.querySelectorAll("input[name='type']");
+
+// Amount
+const amountInput = document.getElementById("amount");
+
+// Cu
+// CATEGORY
+const categoryInput = document.getElementById;
